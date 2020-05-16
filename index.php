73aa66aadfd3c9648a38fed5656ec44b9fd381bf
L200180079 <rahmat.beny12@gmail.com>
2020-05-17 06:30:47 +0700

    mengganti foto admin

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ if ($_SESSION['status'] != "sudah_login") {
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['nama']; ?></span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                <img class="img-profile rounded-circle" src="https://img.icons8.com/bubbles/2x/admin-settings-male.png">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
